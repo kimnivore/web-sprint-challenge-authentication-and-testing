48e086481a49c6ac09ec16eab8973e44152fe2a8
Kim Nguyen <kimnivore@gmail.com>
2022-02-24 17:38:00 -0800

    completed middleware

--- api/middleware/restricted.js
+++ api/middleware/restricted.js
@@ -1,5 +1,21 @@
+const jwt = require('jsonwebtoken');
+const { JWT_SECRET } = require('../secrets/index');
+const Jokes = require('../jokes/jokes-model');
+
 module.exports = (req, res, next) => {
-  next();
+  const token = req.headers.authorization;
+  if (!token) {
+    next({ status: 401, message: 'token required' });
+  } else {
+    jwt.verify(token, JWT_SECRET, (err, decodedToken) => {
+      if(err) {
+        next({ status: 401, message: 'token invalid' })
+      }else {
+        req.decodedJWT = decodedToken;
+        next()
+      }
+    })
+  }
   /*
     IMPLEMENT
 
