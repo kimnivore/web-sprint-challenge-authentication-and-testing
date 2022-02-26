77e43805e58ecdd8090b9bc6f8211bd380236588
Kim Nguyen <kimnivore@gmail.com>
2022-02-25 16:34:05 -0800

    wip

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -21,7 +21,6 @@ router.post('/register', validateUsername, validateData, async (req, res, next) 
 
 
 router.post('/login', checkUsernameExists, validateData, (req, res, next) => {
-
   let {username, password} = req.body
   Users.findBy({ username })
     .then((user) => {
